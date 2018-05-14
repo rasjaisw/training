fb1e62be3cbee18c7db6b51e1168ce398c3ac8f9
rasjaisw <rashmi.jaiswal@capgemini.com>
2018-05-14 12:47:49 +0530

    reverse file added

---
+++ Reverse/Reverse.java
@@ -0,0 +1,37 @@
+/**
+* The program displays reverse string
+* @author  Rashmi Jaiswal
+* @date 14/5/18 
+**/
+
+//	Java program to Reverse a String using swapping
+	// of variables
+	import java.lang.*;
+	import java.io.*;
+	import java.util.*;
+	 
+	
+	public class Reverse {
+
+	    public static void main(String[] args)
+	    {
+	        String input = "Let's take LeetCode contest";
+	        char[] temparray = input.toCharArray();
+	        int left, right=0;
+	        right = temparray.length-1;
+	 
+	        for (left=0; left < right ; left++ ,right--)
+	        {
+	            // Swap values of left and right
+	            char temp = temparray[left];
+	            temparray[left] = temparray[right];
+	            temparray[right]=temp;
+	        }
+	 
+	        for (char c : temparray)
+	            System.out.print(c);
+	        System.out.println();
+	    }
+	}
+
+
