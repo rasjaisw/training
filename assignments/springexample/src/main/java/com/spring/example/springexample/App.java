cc752648a8d304764057215a60cd4ef1c5cc0232
rasjaisw <rashmi.jaiswal@capgemini.com>
2018-05-22 16:01:01 +0530

    spring application created

---
+++ assignments/springexample/src/main/java/com/spring/example/springexample/App.java
@@ -0,0 +1,17 @@
+package com.spring.example.springexample;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+    	
+    	Helloworld helloworld = (Helloworld)context.getBean("helloworld");
+    	
+        helloworld.sayHello();
+    }
+}
---
+++ assignments/springexample/src/main/java/com/spring/example/springexample/Helloworld.java
@@ -0,0 +1,21 @@
+package com.spring.example.springexample;
+
+public class Helloworld {
+
+	private String username;
+
+	
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void sayHello()
+	{
+		
+		System.out.println("hello"+username);
+		
+		
+	}
+
+}
